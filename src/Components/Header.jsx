c96901dbefe04e88b1a57a89b9a53a1cb9411262
HarshThakur1206 <thakurharsh201@gmail.com>
2024-02-04 21:09:29 -1200

    Initial Commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from "../assets/investment-calculator-logo.png"
+const Header = () => {
+  return (
+    <header id='header'>
+        <img src={logo} alt="Logo Showing money bank"/>
+        <h1>Incestment Calculator</h1>  
+    </header>
+  )
+}
+
+export default Header
